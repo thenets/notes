f4ed612bc549421ba36b65d8dfbc110fb9ab05f7
Luiz Costa <luizcosta@redhat.com>
2024-09-25 04:31:24 -0300

    core features working

--- kvstore/kvstore.go
+++ kvstore/kvstore.go
@@ -8,7 +8,7 @@ import (
 // KeyValueStore defines the interface for a key-value store.
 type KeyValueStore interface {
 	Set(key string, value interface{}) error
-	Get(key string) (interface{}, error)
+	Get(key string) (string, error)
 	Delete(key string) error
 	Exists(key string) bool
 }
@@ -18,19 +18,23 @@ type InMemoryKVStore struct {
 	store sync.Map
 }
 
-// Set sets a value for a given key.
+// Set sets a value for a given key. This is kept for backward compatibility and can be removed if not needed.
 func (kv *InMemoryKVStore) Set(key string, value interface{}) error {
 	kv.store.Store(key, value)
 	return nil
 }
 
-// Get retrieves the value for a given key.
-func (kv *InMemoryKVStore) Get(key string) (interface{}, error) {
+// Get retrieves the string value for a given key.
+func (kv *InMemoryKVStore) Get(key string) (string, error) {
 	value, found := kv.store.Load(key)
 	if !found {
-		return nil, fmt.Errorf("key not found")
+		return "", fmt.Errorf("key not found")
 	}
-	return value, nil
+	strValue, ok := value.(string)
+	if !ok {
+		return "", fmt.Errorf("value is not a string")
+	}
+	return strValue, nil
 }
 
 // Delete deletes a key-value pair.
--- main.go
+++ main.go
@@ -1,54 +1,132 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/thenets/notes/kvstore"
 )
 
-func pageApiNoteGet(w http.ResponseWriter, r *http.Request) {
-    // Extract the key from the URL path
-    keys := r.URL.Path[len("/api/"):]
-    if keys == "" {
-        http.Error(w, "Key is required", http.StatusBadRequest)
-        return
-    }
+var kv = &kvstore.InMemoryKVStore{}
 
-    // Print the key to the console
-    fmt.Println("Received key:", keys)
-
-    // Send a response back to the client
-    w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "Key received: %s", keys)
+type Note struct {
+	Content   string `json:"content"`
+	UpdatedAt string `json:"updated_at"`
+	Error     string `json:"error"`
 }
 
+func getCurrentDatetime() string {
+	now := time.Now().UTC()
+	iso8601Format := now.Format(time.RFC3339)
+	return iso8601Format
+}
 
-func pageHomepage(w http.ResponseWriter, r *http.Request) {
-	// Create a new instance of InMemoryKVStore
-	kv := &kvstore.InMemoryKVStore{}
+func pageApiNoteGet(w http.ResponseWriter, r *http.Request) {
+	// Extract the key from the URL path
+	keys := r.URL.Path[len("/api/"):]
+	if keys == "" {
+		http.Error(w, "Key is required", http.StatusBadRequest)
+		return
+	}
+	if strings.Count(keys, "/") > 0 {
+		http.Error(w, "Invalid key format. Key must be in the form /api/{note_id}", http.StatusBadRequest)
+		return
+	}
 
-	// Set a value
-	err := kv.Set("name", "Alice")
+	// Update value
+	fmt.Println("RETRIEVE:", keys)
+	before_value, err := kv.Get(keys)
+	if err != nil {
+		fmt.Println("Error getting key:", err)
+	}
+	if err != nil {
+		fmt.Println("Setting empty value to it.")
+		before_value = ""
+	}
+	fmt.Println("  - before:", before_value)
+	after_value := before_value + "+"
+
+	err = kv.Set(keys, after_value)
 	if err != nil {
 		fmt.Println("Error setting key:", err)
 		return
 	}
+	fmt.Println("  - after:", after_value)
 
-	// Get the value
-	value, err := kv.Get("name")
+	// Send a response back to the client
+	note := Note{
+		Content:   after_value,
+		UpdatedAt: getCurrentDatetime(),
+		Error:     "",
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	err = json.NewEncoder(w).Encode(note)
 	if err != nil {
-		fmt.Println("Error getting key:", err)
+		http.Error(w, fmt.Sprintf("Failed to encode JSON: %v", err), http.StatusInternalServerError)
 		return
 	}
-	fmt.Println("Value for 'name':", value)
-
-	http.ServeFile(w, r, "./static/index.html")
 }
 
+func staticFileHandler(w http.ResponseWriter, r *http.Request) {
+	keys := r.URL.Path[len("/"):]
+
+	var keys_slice []string
+
+	// Static pattern identified
+	if len(keys) == 0 || strings.HasPrefix(keys, "assets/") {
+		// Default for homepage
+		if len(keys) == 0 {
+			keys_slice = append(keys_slice, "static")
+			keys_slice = append(keys_slice, "index.html")
+		}
+
+		// Assets
+		if strings.HasPrefix(keys, "assets/") {
+			keys_slice = append(keys_slice, "static")
+			keys_slice = append(keys_slice, strings.Split(keys, "/")...)
+		}
+
+		filePath := filepath.Join(keys_slice...)
+		fmt.Println(filePath)
+
+		// Check if the file exists
+		if _, err := os.Stat(filePath); os.IsNotExist(err) {
+			http.Error(w, "Not Found1", http.StatusNotFound)
+			return
+		}
+
+		// Serve the static file
+		w.Header().Set("Content-Type", "text/html")
+		http.ServeFile(w, r, filePath)
+	}
+
+	// Loading notes UI
+	if len(strings.Split(keys, "/")) > 1 {
+		http.Error(w, "Not Found2", http.StatusNotFound)
+		return
+	}
+	keys_slice = append(keys_slice, "static")
+	keys_slice = append(keys_slice, "note.html")
+	filePath := filepath.Join(keys_slice...)
+	fmt.Println(filePath)
+	w.Header().Set("Content-Type", "text/html")
+	http.ServeFile(w, r, filePath)
+
+}
+
+// func pageHomepage(w http.ResponseWriter, r *http.Request) {
+// 	http.ServeFile(w, r, "./static/index.html")
+// }
+
 func main() {
-	http.HandleFunc("/", pageHomepage)
 	http.HandleFunc("/api/", pageApiNoteGet)
+	http.HandleFunc("/", staticFileHandler)
 	fmt.Println("Starting server at port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic(err)
