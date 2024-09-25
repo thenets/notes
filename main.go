248334e09259e97533b1ce1a8bb2598af96b04d0
Luiz Costa <luizcosta@redhat.com>
2024-09-25 05:21:58 -0300

    before many changes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -20,56 +21,82 @@ type Note struct {
 	Error     string `json:"error"`
 }
 
+type ResponseNotePost struct {
+	Message string `json:"message"`
+}
+
 func getCurrentDatetime() string {
 	now := time.Now().UTC()
 	iso8601Format := now.Format(time.RFC3339)
 	return iso8601Format
 }
 
-func pageApiNoteGet(w http.ResponseWriter, r *http.Request) {
-	// Extract the key from the URL path
+func pageApiNote(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost && r.Method != http.MethodGet {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+		return
+	}
+
 	keys := r.URL.Path[len("/api/"):]
-	if keys == "" {
-		http.Error(w, "Key is required", http.StatusBadRequest)
-		return
-	}
-	if strings.Count(keys, "/") > 0 {
-		http.Error(w, "Invalid key format. Key must be in the form /api/{note_id}", http.StatusBadRequest)
-		return
+
+	if r.Method == http.MethodGet {
+		// Extract the key from the URL path
+		if keys == "" {
+			http.Error(w, "Key is required", http.StatusBadRequest)
+			return
+		}
+		if strings.Count(keys, "/") > 0 {
+			http.Error(w, "Invalid key format. Key must be in the form /api/{note_id}", http.StatusBadRequest)
+			return
+		}
+
+		// Retrieving value
+		fmt.Println("RETRIEVE:", keys)
+		before_value, err := kv.Get(keys)
+		if err != nil {
+			fmt.Println("Error getting key:", err)
+			fmt.Println("Setting empty value to it.")
+			before_value = ""
+		}
+		// fmt.Println("  - before:", before_value)
+
+		// Send a response back to the client
+		note := Note{
+			Content:   before_value,
+			UpdatedAt: getCurrentDatetime(),
+			Error:     "",
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+		err = json.NewEncoder(w).Encode(note)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Failed to encode JSON: %v", err), http.StatusInternalServerError)
+			return
+		}
 	}
 
-	// Update value
-	fmt.Println("RETRIEVE:", keys)
-	before_value, err := kv.Get(keys)
-	if err != nil {
-		fmt.Println("Error getting key:", err)
-	}
-	if err != nil {
-		fmt.Println("Setting empty value to it.")
-		before_value = ""
-	}
-	fmt.Println("  - before:", before_value)
-	after_value := before_value + "+"
+	if r.Method == http.MethodPost {
+		dataRawBytes, _ := io.ReadAll(r.Body)
+		dataRawString := string(dataRawBytes)
+		fmt.Println(dataRawString)
+		if !strings.HasPrefix(dataRawString, "note=") {
+			http.Error(w, "The POST body has an invalid format.", http.StatusInternalServerError)
+			return
+		}
 
-	err = kv.Set(keys, after_value)
-	if err != nil {
-		fmt.Println("Error setting key:", err)
-		return
-	}
-	fmt.Println("  - after:", after_value)
+		// Update value
+		dataString := dataRawString[5:]
+		err := kv.Set(keys, dataString)
+		if err != nil {
+			fmt.Println("Error setting key:", err)
+			return
+		}
 
-	// Send a response back to the client
-	note := Note{
-		Content:   after_value,
-		UpdatedAt: getCurrentDatetime(),
-		Error:     "",
-	}
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(note)
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Failed to encode JSON: %v", err), http.StatusInternalServerError)
-		return
+		responseData := ResponseNotePost{
+			Message: dataString,
+		}
+
+		json.NewEncoder(w).Encode(responseData)
 	}
 }
 
@@ -97,7 +124,7 @@ func staticFileHandler(w http.ResponseWriter, r *http.Request) {
 
 		// Check if the file exists
 		if _, err := os.Stat(filePath); os.IsNotExist(err) {
-			http.Error(w, "Not Found1", http.StatusNotFound)
+			http.Error(w, "Not Found", http.StatusNotFound)
 			return
 		}
 
@@ -108,7 +135,7 @@ func staticFileHandler(w http.ResponseWriter, r *http.Request) {
 
 	// Loading notes UI
 	if len(strings.Split(keys, "/")) > 1 {
-		http.Error(w, "Not Found2", http.StatusNotFound)
+		http.Error(w, "Not Found", http.StatusNotFound)
 		return
 	}
 	keys_slice = append(keys_slice, "static")
@@ -120,12 +147,8 @@ func staticFileHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// func pageHomepage(w http.ResponseWriter, r *http.Request) {
-// 	http.ServeFile(w, r, "./static/index.html")
-// }
-
 func main() {
-	http.HandleFunc("/api/", pageApiNoteGet)
+	http.HandleFunc("/api/", pageApiNote)
 	http.HandleFunc("/", staticFileHandler)
 	fmt.Println("Starting server at port 8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
